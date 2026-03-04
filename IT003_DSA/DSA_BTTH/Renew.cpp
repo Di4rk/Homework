ef5ddb63990ff1fa06acaf19ec2080ff449f67f0
Diark <25520458@gm.uit.edu.vn>
2026-03-04 23:00:17 +0700

    DSA Thuc hanh

--- IT003_DSA/DSA_BTTH/Renew.cpp
+++ IT003_DSA/DSA_BTTH/Renew.cpp
@@ -1,17 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
+bool check(long long x, long long a, long long k, long long b ,long long m, long long n){
+    long long daya = x - x / k;
+    long long dayb = x - x / m;
+    
+    __int128 total = (__int128)daya * a + (__int128)dayb * b;
+    return total >= n;
+}
 int main(){
-    long a,k,b,m,n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long a,k,b,m,n;
     cin >> a >> k >> b >> m >> n; 
-    long i = 0, sum = 0;
-    while(true){
-        if(sum >= n){
-            cout << i;
-            return 0;
-        }        
-        i++;
-        if(i % k != 0) sum += a;
-        if(i % m != 0) sum += b;
+    long long left = 0, right = LLONG_MAX, midle;
+    long long ans = midle;
+    while(left <= right){
+        midle = (left + right) /2;
+        if(check(midle,a,k,b,m,n)){
+            ans = midle;
+            right = midle -1;
+        }
+        else{
+            left = midle + 1;
+        }
     }
+    cout << ans;
     return 0;
 }
