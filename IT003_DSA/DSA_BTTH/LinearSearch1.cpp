326d842cea8f1575a2034094760db20a977f23b0
Diark <25520458@gm.uit.edu.vn>
2026-03-05 23:40:45 +0700

    Fixing

--- IT003_DSA/DSA_BTTH/LinearSearch1.cpp
+++ IT003_DSA/DSA_BTTH/LinearSearch1.cpp
@@ -4,30 +4,8 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-
     vector<int> a(n);
-    for(int i = 0; i < n; i++)
-        cin >> a[i];
-
+    for(int &k : a) cin >> k;
     int x;
     cin >> x;
-
-    int pos = -1;
-    for(int i = 0; i < n; i++){
-        if(a[i] == x){
-            pos = i;
-            break;
-        }
-    }
-
-    if(pos == -1){
-        cout << -1;
-        return 0;
-    }
-    cout << pos << endl;            
-    cout << pos + 1 << endl;         
-    cout << n - 1 - pos << endl;     
-    cout << n - pos << endl;
-
-    return 0;
 }
