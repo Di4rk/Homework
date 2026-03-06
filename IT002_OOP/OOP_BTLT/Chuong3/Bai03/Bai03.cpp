02c2895fc50fd1336ed6dfacff7060a109ced3b8
Diark <25520458@gm.uit.edu.vn>
2026-03-06 23:09:05 +0700

    Class Khoang cach 2 diem

---
+++ IT002_OOP/OOP_BTLT/Chuong3/Bai03/Bai03.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Diem {
+private:
+	float x, y;
+public:
+	void Nhap();
+	void Xuat();
+	float KhoangCach(Diem other);
+};
+
+void Diem::Nhap() {
+	cout << "Nhap x: ";
+	cin >> x;
+	cout << "Nhap y: ";
+	cin >> y;
+}
+
+float Diem::KhoangCach(Diem other) {
+	return sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
+}
+
+void Diem::Xuat() {
+	cout << "Diem: (" << x << ", " << y << ")" << endl;
+}
+
+int main() {
+	Diem p1, p2;
+	cout << "Nhap diem thu nhat: " << endl;
+	p1.Nhap();
+	cout << "Nhap diem thu hai: " << endl;
+	p2.Nhap();
+	cout << "Khoang cach giua hai diem: " << p1.KhoangCach(p2) << endl;
+}
